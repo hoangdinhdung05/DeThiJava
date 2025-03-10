0137a7516c7cefe742401cf5ab7c2e4bc5bd1317
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2025-03-10 10:55:14 +0700

    Done De 1

--- Test.java
+++ Test.java
@@ -1,64 +1,29 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+class Msg {
+    public Msg() {
+        System.out.println("Hello");
+    }
+    public void sayGoodbye() {
+        System.out.println("Goodbye");
+    }
+}
+class TestMsg extends Msg {
+    public TestMsg() {
+        super(); 
+        System.out.println("Hello Java");
+    }
+    public void sayGoodbye() {
+        System.out.println("Goodbye Java");
+    }
+}
+
 
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-
-        //Tao file input va output
-        String inputFile = "input.txt";
-        String outputFile = "output.txt";
-
-        File file = new File(inputFile);
-        //check loi 
-        if(!file.exists() || file.length() == 0) {
-            try {
-                FileWriter writer = new FileWriter(file);
-                writer.write("Hello World!");
-                writer.flush();
-                System.out.println("File create successfully!");
-                writer.close();
-            } catch (IOException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
-
-        //Doc file input chuyen qua output
-        try (FileInputStream input = new FileInputStream(inputFile);
-            FileOutputStream output = new FileOutputStream(outputFile)) {
-
-            int c;
-            while ((c = input.read()) != -1) {
-                output.write(c);
-            }
-            output.flush(); //dam bao du lieu duoc ghi vao file
-
-            System.out.println("File copied successfully!");
-
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-        //Hien thi noi dung file output
-        System.out.println("Content of " + outputFile + ":");
-        try {
-            String line;
-            while((line = sc.nextLine()) != null) {
-                System.out.println(line);
-            }
-
-        } catch (InputMismatchException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-        sc.close();
-
+        Msg m1 = new Msg(); //khởi tạo in ra hello
+        Msg m2 = new TestMsg(); //kết thừa lại hello và in ra hello cùng với hello java
+        m1.sayGoodbye();
+        m2.sayGoodbye();
 
     }
 
